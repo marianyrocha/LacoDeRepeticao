2659ec864fdb8d42a87e15ab86fe895e77e7f374
marianyrocha <152741522+marianyrocha@users.noreply.github.com>
2024-06-05 19:05:52 -0400

    Add files via upload

---
+++ ex6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Exercício 6:
+
+int n  = 0;
+int soma = 0;
+
+Console.WriteLine("Digite 0 para sair.");
+
+do
+{
+    Console.WriteLine("Digite um número negativo para somar: ");
+    n = int.Parse(Console.ReadLine());
+
+    if (n < 0)
+    {
+        soma = soma + n;
+    }
+
+
+} while (n!=0);
+
+Console.WriteLine("O total da soma dos números negativos é: "+soma);
